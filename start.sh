224feae688cc1882edde0c0c3a31fe58cd553057
leemusic <47916227+leemusic@users.noreply.github.com>
2021-12-03 12:55:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=32a58ab5-7cea-4bee-a76d-8cd48d656ae9
+AUUID=05d31c93-26f1-4550-b5a2-50636730afda
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/jth445600/HerokuXray/main/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/jth445600/HerokuXray/master/etc/xray.json
